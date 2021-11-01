cdff541bb0fef8d81eee0705c6b7188f83ecf7c6
Xavier Coubez <xavier.coubez@cern.ch>
2021-11-01 19:11:12 +0100

    Model for list Items and associated migrations

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User goes to check the homepage
         self.browser.get('http://localhost:8000')
@@ -29,18 +34,23 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # User types "New item to the list" into a text box)
-        inputbox.send_keys('New item to the list')
+        inputbox.send_keys('First item of the list')
 
         # User hits enter, the page updates, and not the page lists item
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == 'New item to the list' for row in rows),
-            "New to-do item did not appear in table",
-        )
+        self.check_for_row_in_list_table('1: First item of the list')
+
+        # User is still invited to add another item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Second item of the list')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)        
+
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: First item of the list')
+        self.check_for_row_in_list_table('2: Second item of the list')
 
         # ...
         self.fail('Finish the test!')
