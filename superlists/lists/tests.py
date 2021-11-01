4e7e4ec5570ed6a8bfbed2f6517a34317f3efe1f
Xavier Coubez <xavier.coubez@cern.ch>
2021-11-01 15:19:51 +0100

    Refactor home page view to use a template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -7,14 +7,7 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_root_urel_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
